f3c4d776001491f17a13323142332e593a5c2f65
Scarpy19 <nootravez19@gmail.com>
2024-12-30 11:00:05 +0100

    fks and nullables

--- db/config.ts
+++ db/config.ts
@@ -3,13 +3,13 @@ import { defineDb, defineTable, column } from 'astro:db';
 const Instituto = defineTable({
   columns: {
     instituto_id: column.number({ primaryKey: true }),
-    nombre: column.text({ optional: false }),
+    nombre: column.text(),
     descripcion: column.text({ optional: true }),
     direccion: column.text({ optional: true }),
     sitio_web: column.text({ optional: true }),
     telefono: column.text({ optional: true }),
     comentarios: column.text({ optional: true }),
-    fecha_creacion: column.date({ optional: false }),
+    fecha_creacion: column.date({ default: new Date() }),
     fecha_actualizacion: column.date({ optional: true })
   }
 });
@@ -17,26 +17,27 @@ const Instituto = defineTable({
 const Grupo = defineTable({
   columns: {
     grupo_id: column.number({ primaryKey: true }),
-    nombre: column.text({ optional: false }),
+    nombre: column.text(),
     descripcion: column.text({ optional: true }),
     comentarios: column.text({ optional: true }),
-    fecha_creacion: column.date({ optional: false }),
+    fecha_creacion: column.date({ default: new Date() }),
     fecha_actualizacion: column.date({ optional: true }),
-    instituto_id: column.number({ optional: false, references: () => Instituto.columns.instituto_id })
+    // FKs
+    instituto_id: column.number({ references: () => Instituto.columns.instituto_id })
   }
 });
 
 const Curso = defineTable({
   columns: {
     curso_id: column.number({ primaryKey: true }),
-    anio_academico: column.text({ optional: false })
+    anio_academico: column.text()
   }
 });
 
 const Alumno = defineTable({
   columns: {
     alumno_id: column.number({ primaryKey: true }),
-    nombre: column.text({ optional: false }),
+    nombre: column.text(),
     apellidos: column.text({ optional: true }),
     telefono: column.text({ optional: true }),
     direccion: column.text({ optional: true }),
@@ -46,8 +47,8 @@ const Alumno = defineTable({
     nia: column.text({ optional: true }),
     nuss: column.text({ optional: true }),
     comentarios: column.text({ optional: true }),
-    activo: column.boolean({ optional: false, default: true }),
-    fecha_creacion: column.date({ optional: false }),
+    activo: column.boolean({ default: true }),
+    fecha_creacion: column.date({ default: new Date() }),
     fecha_actualizacion: column.date({ optional: true })
   }
 });
@@ -55,24 +56,31 @@ const Alumno = defineTable({
 const Inscripcion = defineTable({
   columns: {
     inscripcion_id: column.number({ primaryKey: true }),
-    fecha_inscripcion: column.date({ optional: false }),
+    fecha_inscripcion: column.date(),
     fecha_actualizacion: column.date({ optional: true }),
-    alumno_id: column.number({ optional: false, references: () => Alumno.columns.alumno_id }),
-    grupo_id: column.number({ optional: false, references: () => Grupo.columns.grupo_id }),
-    curso_id: column.number({ optional: false, references: () => Curso.columns.curso_id })
-  }
+    // FKs
+    alumno_id: column.number(),
+    grupo_id: column.number(),
+    curso_id: column.number()
+  },
+  foreignKeys: [
+    {
+      columns: ["alumno_id", "grupo_id", "curso_id"],
+      references: () => [Alumno.columns.alumno_id, Grupo.columns.grupo_id, Curso.columns.curso_id]
+    }
+  ]
 });
 
 const Profesor = defineTable({
   columns: {
     profesor_id: column.number({ primaryKey: true }),
-    nombre: column.text({ optional: false }),
+    nombre: column.text(),
     apellidos: column.text({ optional: true }),
     telefono: column.text({ optional: true }),
     nif: column.text({ optional: true }),
     nip: column.text({ optional: true }),
     comentarios: column.text({ optional: true }),
-    fecha_creacion: column.date({ optional: false }),
+    fecha_creacion: column.date({ default: new Date() }),
     fecha_actualizacion: column.date({ optional: true })
   }
 });
@@ -80,22 +88,30 @@ const Profesor = defineTable({
 const ProfesorInstituto = defineTable({
   columns: {
     profesor_instituto_id: column.number({ primaryKey: true }),
-    fecha_creacion: column.date({ optional: false }),
+    fecha_creacion: column.date({ default: new Date() }),
     fecha_actualizacion: column.date({ optional: true }),
-    profesor_id: column.number({ unique: true, optional: false, references: () => Profesor.columns.profesor_id }),
-    instituto_id: column.number({ optional: false, references: () => Instituto.columns.instituto_id }),
-    curso_id: column.number({ unique: true, optional: false, references: () => Curso.columns.curso_id })
-  }
+    // FKs
+    profesor_id: column.number({ unique: true }),
+    instituto_id: column.number(),
+    curso_id: column.number({ unique: true })
+  },
+  foreignKeys: [
+    {
+      columns: ["profesor_id", "instituto_id", "curso_id"],
+      references: () => [Profesor.columns.profesor_id, Instituto.columns.instituto_id, Curso.columns.curso_id]
+    }
+  ]
 });
 
 const Usuario = defineTable({
   columns: {
     usuario_id: column.number({ primaryKey: true }),
-    usuario: column.text({ optional: false, unique: true }),
-    email: column.text({ optional: false, unique: true }),
-    contrasena: column.text({ optional: false }),
-    rol: column.enum(['Prof', 'Admin'], { optional: false, default: 'Prof' }),
-    activo: column.boolean({ optional: false, default: true }),
+    usuario: column.text({ unique: true }),
+    email: column.text({ unique: true }),
+    contrasena: column.text(),
+    rol: column.text({ default: 'Profesor', }), // "Profesor", "Administrador"
+    activo: column.boolean({ default: true }),
+    // FKs
     profesor_id: column.number({ unique: true, references: () => Profesor.columns.profesor_id })
   }
 });
@@ -103,7 +119,7 @@ const Usuario = defineTable({
 const Empresa = defineTable({
   columns: {
     empresa_id: column.number({ primaryKey: true }),
-    nombre: column.text({ optional: false }),
+    nombre: column.text(),
     nombre_empresarial: column.text({ optional: true }),
     direccion: column.text({ optional: true }),
     cif: column.text({ optional: true }),
@@ -111,8 +127,8 @@ const Empresa = defineTable({
     sector: column.text({ optional: true }),
     tecnologias: column.text({ optional: true }),
     comentarios: column.text({ optional: true }),
-    activo: column.boolean({ optional: false, default: true }),
-    fecha_creacion: column.date({ optional: true }),
+    activo: column.boolean({ default: true }),
+    fecha_creacion: column.date({ default: new Date() }),
     fecha_actualizacion: column.date({ optional: true })
   }
 });
@@ -122,27 +138,34 @@ const Practica = defineTable({
     practica_id: column.number({ primaryKey: true }),
     fecha_inicio: column.date({ optional: true }),
     fecha_fin: column.date({ optional: true }),
-    tipo_practica: column.enum(['Normal', 'Extraordinaria'], { optional: false, default: 'Normal' }),
-    estado: column.enum(['Pendiente', 'En curso', 'Finalizada', 'Cancelada'], { optional: false, default: 'Pendiente' }),
+    tipo_practica: column.text({ default: 'Normal' }), // "Normal", "Extraordinaria"
+    estado: column.text({ default: 'Pendiente' }), // "Pendiente", "En curso", "Finalizada", "Cancelada"
     comentarios: column.text({ optional: true }),
-    fecha_creacion: column.date({ optional: true }),
+    fecha_creacion: column.date({ default: new Date() }),
     fecha_actualizacion: column.date({ optional: true }),
-    alumno_id: column.number({ unique: true, optional: false, references: () => Alumno.columns.alumno_id }),
-    profesor_id: column.number({ optional: false, references: () => Profesor.columns.profesor_id }),
-    empresa_id: column.number({ optional: false, references: () => Empresa.columns.empresa_id }),
-    curso_id: column.number({ unique: true, optional: false, references: () => Curso.columns.curso_id })
-  }
+    // FKs
+    alumno_id: column.number({ unique: true }),
+    profesor_id: column.number(),
+    empresa_id: column.number(),
+    curso_id: column.number({ unique: true })
+  },
+  foreignKeys: [
+    {
+      columns: ["alumno_id", "profesor_id", "empresa_id", "curso_id"],
+      references: () => [Alumno.columns.alumno_id, Profesor.columns.profesor_id, Empresa.columns.empresa_id, Curso.columns.curso_id]
+    }
+  ]
 });
 
 const Contacto = defineTable({
   columns: {
     contacto_id: column.number({ primaryKey: true }),
-    nombre: column.text({ optional: false }),
+    nombre: column.text(),
     apellidos: column.text({ optional: true }),
     telefono: column.text({ optional: true }),
     email: column.text({ unique: true }),
     comentarios: column.text({ optional: true }),
-    fecha_creacion: column.date({ optional: true }),
+    fecha_creacion: column.date({ default: new Date() }),
     fecha_actualizacion: column.date({ optional: true })
   }
 });
@@ -151,11 +174,18 @@ const ContactoEmpresa = defineTable({
   columns: {
     contacto_empresa_id: column.number({ primaryKey: true }),
     puesto: column.text({ optional: true }),
-    fecha_creacion: column.date({ optional: true }),
+    fecha_creacion: column.date({ default: new Date() }),
     fecha_actualizacion: column.date({ optional: true }),
-    contacto_id: column.number({ unique: true, optional: false, references: () => Contacto.columns.contacto_id }),
-    empresa_id: column.number({ unique: true, optional: false, references: () => Empresa.columns.empresa_id })
-  }
+    // FKs
+    contacto_id: column.number({ unique: true }),
+    empresa_id: column.number({ unique: true })
+  },
+  foreignKeys: [
+    {
+      columns: ["contacto_id", "empresa_id"],
+      references: () => [Contacto.columns.contacto_id, Empresa.columns.empresa_id]
+    }
+  ]
 });
 
 const Interaccion = defineTable({
@@ -163,24 +193,32 @@ const Interaccion = defineTable({
     interaccion_id: column.number({ primaryKey: true }),
     descripcion: column.text({ optional: true }),
     tipo: column.text({ optional: true }),
-    estado: column.enum(['Pendiente', 'Cancelada', 'Finalizada'], { optional: false, default: 'Pendiente' }),
+    estado: column.text({ default: 'Pendiente' }), // "Pendiente", "Cancelada", "Finalizada"
     comentarios: column.text({ optional: true }),
     fecha_interaccion: column.date({ optional: true }),
-    fecha_creacion: column.date({ optional: true }),
+    fecha_creacion: column.date({ default: new Date() }),
     fecha_actualizacion: column.date({ optional: true }),
-    empresa_id: column.number({ optional: false, references: () => Empresa.columns.empresa_id }),
-    contacto_id: column.number({ references: () => Contacto.columns.contacto_id })
-  }
+    // FKs
+    empresa_id: column.number(),
+    contacto_id: column.number({ optional: true })
+  },
+  foreignKeys: [
+    {
+      columns: ["empresa_id", "contacto_id"],
+      references: () => [Empresa.columns.empresa_id, Contacto.columns.contacto_id]
+    }
+  ]
 });
 
 const Registro = defineTable({
   columns: {
     registro_id: column.number({ primaryKey: true }),
-    entidad: column.text({ optional: false }),
-    id_entidad: column.number({ optional: false }),
-    descripcion: column.text({ optional: false }),
-    fecha_accion: column.date({ optional: false }),
-    profesor_id: column.number({ optional: false, references: () => Profesor.columns.profesor_id })
+    entidad: column.text(),
+    id_entidad: column.number(),
+    descripcion: column.text(),
+    fecha_accion: column.date({ default: new Date() }),
+    // FKs
+    profesor_id: column.number({ references: () => Profesor.columns.profesor_id })
   }
 });
 
