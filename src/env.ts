a8d6d242e12b60111a3d95a66f6376decac27ed0
Scarpy19 <nootravez19@gmail.com>
2025-01-01 16:49:23 +0100

    node and env

---
+++ src/env.ts
@@ -0,0 +1,14 @@
+const { TURSO_DATABASE_URL, TURSO_AUTH_TOKEN } = import.meta.env;
+
+export function getEnv() {
+
+    if (!TURSO_DATABASE_URL) {
+        throw new Error('Environment variable TURSO_DATABASE_URL is not defined.');
+    }
+
+    if (!TURSO_AUTH_TOKEN) {
+        throw new Error('Environment variable TURSO_AUTH_TOKEN is not defined.');
+    }
+
+    return { TURSO_DATABASE_URL, TURSO_AUTH_TOKEN };
+}
