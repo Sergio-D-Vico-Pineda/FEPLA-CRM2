ea8145b641b2f3b3f5adbbcb3037edd1cdeb5f7f
Scarpy19 <nootravez19@gmail.com>
2025-01-04 17:18:25 +0100

    getEnv fixed

--- src/env.ts
+++ src/env.ts
@@ -1,6 +1,6 @@
 const { TURSO_DATABASE_URL, TURSO_AUTH_TOKEN } = import.meta.env;
 
-export function getEnv() {
+export default function getEnv() {
 
     if (!TURSO_DATABASE_URL) {
         throw new Error('Environment variable TURSO_DATABASE_URL is not defined.');
