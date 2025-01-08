8dee107223baf86b46545a995bb540af34f4a362
Scarpy19 <nootravez19@gmail.com>
2025-01-08 12:29:08 +0100

    new prof

---
+++ src/pages/api/admin/index.js
@@ -0,0 +1,40 @@
+import prisma from "@db/index.js";
+
+async function GET() {
+    return new Response("Example API", { status: 200 });
+}
+
+async function POST({ request }) {
+    const data = await request.json();
+
+    try {
+        const newProf = await prisma.profesor.create({
+            data: {
+                nombre: data.nombre,
+                apellidos: data.apellidos == "" ? null : data.apellidos,
+                telefono: data.telefono == "" ? null : data.telefono,
+                nif: data.nif == "" ? null : data.nif,
+                nip: data.nip == "" ? null : data.nip,
+                comentarios: data.comentarios == "" ? null : data.comentarios,
+                usuario: {
+                    create: {
+                        usuario: data.usuario,
+                        email: data.email,
+                        contrasena: data.contrasena
+                    }
+                }
+            }
+        })
+
+        newProf.message = "Profesor creado exitosamente.";
+
+        return new Response(JSON.stringify(
+            newProf
+        ), { status: 200 });
+    } catch (error) {
+        return new Response(error, { status: 404 });
+    }
+
+}
+
+export { GET, POST };
