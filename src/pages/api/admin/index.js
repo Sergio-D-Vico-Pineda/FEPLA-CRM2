4f03e98dbe3e53a36f837adeb813b3ca31ebdaa7
Scarpy19 <nootravez19@gmail.com>
2025-01-08 13:11:16 +0100

    wip patch

--- src/pages/api/admin/index.js
+++ src/pages/api/admin/index.js
@@ -37,4 +37,40 @@ async function POST({ request }) {
 
 }
 
-export { GET, POST };
+export async function PATCH({ request }) {
+    const data = await request.json();
+
+    try {
+        const newProf = await prisma.profesor.update({
+            where: {
+                id_profesor: data.id_profesor
+            },
+            data: {
+                nombre: data.nombre,
+                apellidos: data.apellidos == "" ? null : data.apellidos,
+                telefono: data.telefono == "" ? null : data.telefono,
+                nif: data.nif == "" ? null : data.nif,
+                nip: data.nip == "" ? null : data.nip,
+                comentarios: data.comentarios == "" ? null : data.comentarios,
+                usuario: {
+                    update: {
+                        usuario: data.usuario,
+                        email: data.email,
+                        contrasena: data.contrasena
+                    }
+                }
+            }
+        })
+
+        newProf.message = "Profesor actualizado exitosamente.";
+
+        return new Response(JSON.stringify(
+            newProf
+        ), { status: 200 });
+
+    } catch (error) {
+        return new Response(error, { status: 404 });
+    }
+}
+
+export { GET, POST, PATCH };
