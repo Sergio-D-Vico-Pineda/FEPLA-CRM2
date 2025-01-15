a95f6c39268ad531db42ccd0a51b192f45477980
Scarpy19 <nootravez19@gmail.com>
2025-01-15 13:05:48 +0100

    update example api

--- src/pages/api/example.js
+++ src/pages/api/example.js
@@ -9,22 +9,39 @@ async function POST({ request }) {
     const user = await prisma.usuario.findFirst();
     console.log(user);
 
-    if (user == null) {
-        return new Response("User not found", { status: 404 });
+    try {
+        return new Response(JSON.stringify(user), { status: 200 });
+    } catch (error) {
+        console.log(error)
+        return new Response(JSON.stringify({
+            message: "User not found"
+        }), { status: 404 });
     }
-
-    return new Response(JSON.stringify(user), { status: 200 });
 }
 
 async function PATCH({ request }) {
     const data = await request.json();
 
-    return new Response(JSON.stringify(data), { status: 200 });
+    try {
+        return new Response(JSON.stringify(user), { status: 200 });
+    } catch (error) {
+        console.log(error)
+        return new Response(JSON.stringify({
+            message: "User not found"
+        }), { status: 404 });
+    }
 }
 
 async function PUT({ request }) {
     const data = await request.json();
-    return new Response(JSON.stringify(data), { status: 200 });
+    try {
+        return new Response(JSON.stringify(user), { status: 200 });
+    } catch (error) {
+        console.log(error)
+        return new Response(JSON.stringify({
+            message: "User not found"
+        }), { status: 404 });
+    }
 }
 
 export { GET, POST, PATCH, PUT };
