9aae2672b3c6f82f8304d7ef7b751023e6cc82ca
Scarpy19 <nootravez19@gmail.com>
2025-01-16 20:12:43 +0100

    add and remove comments

--- src/pages/api/instituto/index.js
+++ src/pages/api/instituto/index.js
@@ -62,17 +62,4 @@ async function PATCH({ request }) {
     }
 }
 
-/* async function PUT({ request }) {
-    const data = await request.json();
-    try {
-        return new Response(JSON.stringify(), { status: 200 });
-    } catch (error) {
-        console.log(error)
-        return new Response(JSON.stringify({
-            message: "User not found"
-        }), { status: 404 });
-    }
-}
- */
-
 export { GET, POST, PATCH };
