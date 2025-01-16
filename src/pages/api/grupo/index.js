52334a7998ee595f82370ff864f62c7bbc610e95
Sergio David Vico Pineda <servicpin2@alu.edu.gva.es>
2025-01-16 16:47:25 +0100

    api grupo WIP and instituto

---
+++ src/pages/api/grupo/index.js
@@ -0,0 +1,59 @@
+import prisma from "@db/index.js";
+import createRegistro from "../utils.js";
+
+async function GET() {
+    return new Response("Grupo API", { status: 200 });
+}
+
+async function POST({ request }) {
+    const data = await request.json();
+
+    try {
+        const newGroup = await prisma.grupo.create({
+            data: {
+                nombre: data.nombre,
+                descripcion: data.descripcion,
+                comentarios: data.comentarios,
+                id_instituto: data.id_instituto
+            }
+        })
+
+        const log = await createRegistro(prisma, { id_entidad: newGroup.id_grupo, id_profesor: data.id_active_user }, "Grupo", "creación de grupo");
+        console.log(log);
+
+        newGroup.message = "Grupo creado exitosamente.";
+        return new Response(JSON.stringify(newGroup), { status: 200 });
+    } catch (error) {
+        console.log(error)
+        return new Response(JSON.stringify({
+            message: "Grupo no creado."
+        }), { status: 404 });
+    }
+}
+
+async function PATCH({ request }) {
+    const data = await request.json();
+
+    try {
+        return new Response(JSON.stringify(user), { status: 200 });
+    } catch (error) {
+        console.log(error)
+        return new Response(JSON.stringify({
+            message: "User not found"
+        }), { status: 404 });
+    }
+}
+
+async function PUT({ request }) {
+    const data = await request.json();
+    try {
+        return new Response(JSON.stringify(user), { status: 200 });
+    } catch (error) {
+        console.log(error)
+        return new Response(JSON.stringify({
+            message: "User not found"
+        }), { status: 404 });
+    }
+}
+
+export { GET, POST, PATCH, PUT };
---
+++ src/pages/api/instituto/index.js
@@ -0,0 +1,78 @@
+import prisma from "@db/index.js";
+import createRegistro from "../utils";
+
+async function GET() {
+    return new Response("Instituto API", { status: 200 });
+}
+
+async function POST({ request }) {
+    const data = await request.json();
+
+    try {
+
+        const newInst = await prisma.instituto.create({
+            data: {
+                nombre: data.nombre,
+                descripcion: data.descripcion,
+                direccion: data.direccion,
+                sitio_web: data.sitio_web,
+                telefono: data.telefono,
+                comentarios: data.comentarios
+            }
+        })
+
+        const log = createRegistro(prisma, { id_entidad: newInst.id_instituto, id_profesor: data.id_active_user }, "Instituto", "creación de instituto");
+        console.log(log);
+        newInst.message = "Instituto creado exitosamente.";
+
+        return new Response(JSON.stringify(newInst), { status: 200 });
+    } catch (error) {
+        console.log(error)
+        return new Response(JSON.stringify({
+            message: "Instituto no creado"
+        }), { status: 404 });
+    }
+}
+
+async function PATCH({ request }) {
+    const data = await request.json();
+
+    try {
+        const inst = await prisma.instituto.update({
+            where: {
+                id_instituto: data.id_instituto
+            },
+            data: {
+                nombre: data.nombre,
+                descripcion: data.descripcion,
+                direccion: data.direccion,
+                sitio_web: data.sitio_web,
+                telefono: data.telefono,
+                comentarios: data.comentarios
+            }
+        })
+
+        const log = await createRegistro(prisma, { id_entidad: data.id_instituto, id_profesor: data.id_active_user }, "Instituto", "actualización de instituto");
+        console.log(log)
+
+        return new Response(JSON.stringify({ message: "Instituto actualizado exitosamente." }), { status: 200, headers: { "Content-Type": "application/json", }, });
+    } catch (error) {
+        console.log(error)
+        return new Response(JSON.stringify({ message: "Instituto no actualizado" }), { status: 404 });
+    }
+}
+
+/* async function PUT({ request }) {
+    const data = await request.json();
+    try {
+        return new Response(JSON.stringify(), { status: 200 });
+    } catch (error) {
+        console.log(error)
+        return new Response(JSON.stringify({
+            message: "User not found"
+        }), { status: 404 });
+    }
+}
+ */
+
+export { GET, POST, PATCH };
