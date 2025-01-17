2148c055fd238075d12e64d13fa155e71d29f8dc
Scarpy19 <nootravez19@gmail.com>
2025-01-17 12:50:56 +0100

    edit, see and api WIP profile profesor

---
+++ src/pages/api/profesor/index.js
@@ -0,0 +1,46 @@
+import prisma from "@db/index.js";
+import createRegistro from "@utils";
+
+async function GET() {
+    return new Response("Profesor API", { status: 200 });
+}
+
+async function PATCH({ request }) {
+    const data = await request.json();
+
+    console.log(data)
+
+    try {
+
+        /* const prof = await prisma.profesor.update({
+            where: {
+                id_profesor: data.id_profesor
+            },
+            data: {
+                nombre: data.nombre,
+                apellidos: data.apellidos,
+                telefono: data.telefono,
+                nif: data.nif,
+                nip: data.nip,
+                usuario: {
+                    update: {
+                        usuario: data.usuario,
+                        email: data.email,
+                        contrasena: data.contrasena
+                    }
+                }
+            }
+        })
+
+        const log = await createRegistro(prisma, { id_entidad: data.id_profesor, id_profesor: data.id_active_user }, "Profesor", "actualización de perfil de un profesor"); */
+
+        return new Response(JSON.stringify({ message: "Perfil actualizado exitosamente." }), { status: 200 });
+    } catch (error) {
+        console.log(error)
+        return new Response(JSON.stringify({
+            message: "Perfil no actualizado"
+        }), { status: 404 });
+    }
+}
+
+export { GET, PATCH };
