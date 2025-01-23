e0522672340eef255792b4ce484535c1eded638c
Sergio David Vico Pineda <servicpin2@alu.edu.gva.es>
2025-01-23 16:02:43 +0100

    API Interacción done

--- src/pages/api/interaccion/index.js
+++ src/pages/api/interaccion/index.js
@@ -37,6 +37,23 @@ async function PATCH({ request }) {
     const data = await request.json();
 
     try {
+        const inter = await prisma.interaccion.update({
+            where: {
+                id_interaccion: data.id_interaccion
+            },
+            data: {
+                descripcion: data.descripcion,
+                tipo: data.tipo,
+                estado: data.estado,
+                comentarios: data.comentarios,
+                fecha_interaccion: data.fecha_interaccion == "" || null ? null : new Date(data.fecha_interaccion),
+                id_contacto: data.id_contacto,
+                id_empresa: data.id_empresa
+            }
+        })
+
+        const log = await createRegistro(prisma, { id_entidad: data.id_interaccion, id_profesor: data.id_active_user }, "Interacción", "actualización de interacción");
+
         return new Response(JSON.stringify({ message: "Interacción actualizada" }), { status: 200 });
     } catch (error) {
         console.log(error)
@@ -44,4 +61,26 @@ async function PATCH({ request }) {
     }
 }
 
-export { GET, POST, PATCH };
+async function DELETE({ request }) {
+    const data = await request.json();
+    try {
+
+        const inter = await prisma.interaccion.delete({
+            where: {
+                id_interaccion: data.id_interaccion
+            }
+        })
+
+        const log = await createRegistro(prisma, { id_entidad: data.id_interaccion, id_profesor: data.id_active_user }, "Interacción", "eliminación de interacción");
+        console.log("INTERACCIÓN ELIMINADA");
+
+        return new Response(JSON.stringify({ message: "Interacción eliminada exitosamente." }), { status: 200 });
+    } catch (error) {
+        console.log(error)
+        return new Response(JSON.stringify({
+            message: "Interacción no eliminada."
+        }), { status: 404 });
+    }
+}
+
+export { GET, POST, PATCH, DELETE };
