f1c027d0ebe3e652b9a5e8a822255ccd6321bd80
Sergio David Vico Pineda <servicpin2@alu.edu.gva.es>
2025-02-11 15:52:09 +0100

    update env

--- src/env.ts
+++ src/env.ts
@@ -2,7 +2,7 @@ const { TURSO_DATABASE_URL, TURSO_AUTH_TOKEN, DATABASE_URL } = import.meta.env;
 
 export default function getEnv() {
 
-    if (!TURSO_DATABASE_URL) {
+    /* if (!TURSO_DATABASE_URL) {
         throw new Error('Environment variable TURSO_DATABASE_URL is not defined.');
     }
 
@@ -12,7 +12,7 @@ export default function getEnv() {
 
     if (!DATABASE_URL) {
         throw new Error('Environment variable DATABASE_URL is not defined.');
-    }
+    } */
 
     return { TURSO_DATABASE_URL, TURSO_AUTH_TOKEN, DATABASE_URL };
 }
